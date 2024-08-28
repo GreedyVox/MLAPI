23bd5b68372c66d12ea9021f494ee6500aa02abc
Lawsy <greedyvox@gmail.com>
2024-08-28 13:10:46 +1000

    Update Character Ai
    Quick update to the Character Ai, for running on server only.

--- Assets/GreedyVox/NetCode/Scripts/Ai/NetCodeAiBD.cs
+++ Assets/GreedyVox/NetCode/Scripts/Ai/NetCodeAiBD.cs
@@ -3,6 +3,7 @@ using BehaviorDesigner.Runtime;
 #endif
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.AI;
 
 /// <summary>
 /// Ai Behavior Designer for running on server only.
@@ -12,11 +13,15 @@ namespace GreedyVox.NetCode.Ai
     [DisallowMultipleComponent]
     public class NetCodeAiBD : NetworkBehaviour
     {
-#if ULTIMATE_CHARACTER_CONTROLLER_MULTIPLAYER_BD_AI
-        private BehaviorTree m_BehaviorTree;
-        private void Awake() => m_BehaviorTree = GetComponent<BehaviorTree>();
         public override void OnNetworkSpawn()
-        { if (m_BehaviorTree != null) m_BehaviorTree.enabled = IsServer; }
+        {
+#if ULTIMATE_CHARACTER_CONTROLLER_MULTIPLAYER_BD_AI
+            if (TryGetComponent<BehaviorTree>(out var tree))
+                tree.enabled = IsServer;
 #endif
+            if (TryGetComponent<NavMeshAgent>(out var agent))
+                agent.enabled = IsServer;
+            base.OnNetworkSpawn();
+        }
     }
 }
